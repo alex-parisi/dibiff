2c1cdb87e00fb606691cbbb4476af46e39c3db25
Alex Parisi <alex.parisi@outlook.com>
2024-07-28 15:07:14 -0400

    Smooth sine waves

--- src/generator/SineGenerator.h
+++ src/generator/SineGenerator.h
@@ -101,4 +101,5 @@ class dibiff::generator::SineGenerator : public dibiff::generator::Generator {
         float frequency;
         int totalSamples;
         int currentSample;
+        float phase;
 };
